71b594ff05dccc7a1f282648b70db944d237cb21
Connor McDonald <129882993+cjvb@users.noreply.github.com>
2024-10-07 19:41:31 -0500

    krish d v2 (servo test)

--- main/arduino_main.cpp
+++ main/arduino_main.cpp
@@ -1,50 +1,25 @@
-/****************************************************************************
-Copyright 2021 Ricardo Quesada
+// Bare minimum code for spinning motors triggered by controller input
 
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
+// Assumes servo is connected to pin 15
 
-    http://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-****************************************************************************/
+// Assumes motor controller IN1 and IN2 are connected to pins 14 and 12
 
 #include "sdkconfig.h"
 #ifndef CONFIG_BLUEPAD32_PLATFORM_ARDUINO
 #error "Must only be compiled when using Bluepad32 Arduino platform"
-#endif  // !CONFIG_BLUEPAD32_PLATFORM_ARDUINO
-
+#endif  !CONFIG_BLUEPAD32_PLATFORM_ARDUINO
 #include <Arduino.h>
 #include <Bluepad32.h>
-
 #include <ESP32Servo.h>
-#include <ESP32SharpIR.h>
-#include <QTRSensors.h>
-
-#include <Arduino_APDS9960.h>
 #include <bits/stdc++.h>
 
-#define APDS9960_INT 2
-#define I2C_SDA 21
-#define I2C_SCL 22
-#define I2C_FREQ 100000
 
-GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
-const int plsensor = 26;
-const int prsensor = 27;
-const int plservo = 32;
-const int prservo = 33;
-
-const double bestdistance = 2;
-const int maxspd = 180;
-const int minspd = 0;
+#define IN1 12
+#define IN2 14
 
 Servo servo;
+
+GamepadPtr myGamepads[BP32_MAX_GAMEPADS];
 
 // This callback gets called any time a new gamepad is connected.
 void onConnectedGamepad(GamepadPtr gp) {
@@ -69,43 +44,54 @@ void onDisconnectedGamepad(GamepadPtr gp) {
     }
 }
 
-
-
-// Arduino setup function. Runs in CPU 1
 void setup() {
-    // Setup the Bluepad32 callbacks
     BP32.setup(&onConnectedGamepad, &onDisconnectedGamepad);
     BP32.forgetBluetoothKeys();
-
+ 
     servo.attach(15);
 
-    Servo.begin(115200);
-
-    ESP32PWM::allocateTimer(0);
-	ESP32PWM::allocateTimer(1);
-	ESP32PWM::allocateTimer(2);
-	ESP32PWM::allocateTimer(3);
-
-    // TODO: Write your setup code here
+    // motor controller outputs
+    pinMode(IN1, OUTPUT);
+    pinMode(IN2, OUTPUT);
+   
+    Serial.begin(115200);
 }
 
-// Arduino loop function. Runs in CPU 1
 void loop() {
     BP32.update();
-
     for (int i = 0; i < BP32_MAX_GAMEPADS; i++) {
-        GamepadPtr myGamepad = myGamepads[i];
-        if (myGamepad && myGamepad->isConnected()) {
-            // TODO: Write your controller code here
-            if(controller->l1() ==1){
+        GamepadPtr controller = myGamepads[i];
+        if (controller && controller->isConnected()) {
+           
+            if (controller->l1() == 1) {
                 Serial.print("Servo move");
                 servo.write(1000);
             }
-            if(controller->l1()==0){
+            if (controller->l1() == 0) {
                 Serial.print("Servo stop");
                 servo.write(1500);
             }
+
+            if(controller->axisRY() > 0) { // negative y is upward on stick
+                Serial.println(" DC motor move");
+                digitalWrite(IN1, LOW);
+                digitalWrite(IN2, HIGH);
+            }
+            if(controller->axisRY() == 0) { // stop motor 1
+                Serial.println(" DC motor stop");
+                digitalWrite(IN1, LOW);
+                digitalWrite(IN2, LOW);
+            }
+
+            // PHYSICAL BUTTON A
+            if (controller->b()) {
+                Serial.println("button a pressed");
+            }
+
         }
+        vTaskDelay(1);
     }
-    vTaskDelay(1);
-}
+}            
+
+
+
